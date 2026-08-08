f32ddab27e60f747740052af4c4cf05c45c16980
Nabzaf <djwildbill@users.noreply.github.com>
2026-08-08 01:42:16 -0400

    Add Heimdall Companion app Gradle config

---
+++ android/HeimdallCompanion/app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.projectodin.heimdall"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.projectodin.heimdall"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {}
